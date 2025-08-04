8029a0992a15f4d031f18d977a6daa8b603bb01c
EvgeniyEmelyanov <jazenja4@gmail.com>
2025-08-04 15:53:53 +0300

    12344567890okjhgfcx

--- src/Povtorenie_s_GPT/HashMap/HashMap_2.java
+++ src/Povtorenie_s_GPT/HashMap/HashMap_2.java
@@ -9,9 +9,11 @@ public class HashMap_2 {
         for (String name : list) {
             int length = name.length();
 
+
             if (!group.containsKey(length)) {
                 group.put(length, new ArrayList<>());
             }
+
             group.get(length).add(name);
 
         }
@@ -20,7 +22,7 @@ public class HashMap_2 {
         return group;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         List<String> names = Arrays.asList("Anna", "Bob", "Alice", "Tom", "Eva", "John");
 
         Map<Integer, List<String>> group = groupNamesBuSort(names);
@@ -28,9 +30,33 @@ public class HashMap_2 {
         for (Map.Entry<Integer, List<String>> entry : group.entrySet()) {
             int len = entry.getKey();
             List<String> list1 = entry.getValue();
+
+            Collections.sort(list1);
+
             System.out.println(len + " символов: " + list1.size() + " имя(ён) → " + list1);
         }
 
+    }
+}
+
+class HashMap_2_1 {
+    public static Map <String, List<String>> groupByFirstLetter(List <String> words) {
+        Map <String, List<String>> group = new TreeMap<>();
+
+        for (String name : words){
+            String letter = name.substring(0,1).toUpperCase();
+
+            if (!group.containsKey(letter)) {
+                group.put(letter, new ArrayList<>());
+            }
+            group.get(letter).add(name);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList(
+                "apple", "banana", "Avocado", "blueberry", "apricot", "Blackberry", "cherry"
+        );
 
     }
 }
