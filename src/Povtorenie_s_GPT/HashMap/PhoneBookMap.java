b36af2530ff59459af4cfcf3050a312d6a366ad8
Evgeniy Emelyanov <jazenja4@gmal.com>
2025-08-05 13:58:37 +0300

    123

--- src/Povtorenie_s_GPT/HashMap/PhoneBookMap.java
+++ src/Povtorenie_s_GPT/HashMap/PhoneBookMap.java
@@ -303,7 +303,7 @@ class StudentsABC {
 
     @Override
     public String toString() {
-        return "Student (" + name + ", " + age + ")";
+        return name + ", " + age;
     }
 }
 
@@ -338,4 +338,240 @@ class GroupStudentsByAgeAndObject {
         }
     }
 
+}
+
+class GroupStudentsByAlphabet {
+
+    public static Map<String, List<StudentsABC>> groupByFirstLetter(List<StudentsABC> students) {
+        Map<String, List<StudentsABC>> group = new HashMap<>();
+
+        for (StudentsABC student : students) {
+            String firstLetter = student.getName().substring(0, 1).toUpperCase();
+            if (!group.containsKey(firstLetter)) {
+                group.put(firstLetter, new ArrayList<>());
+            }
+            group.get(firstLetter).add(student);
+        }
+
+        return group;
+    }
+
+    public static void main(String[] args) {
+        List<StudentsABC> students = Arrays.asList(
+                new StudentsABC("Alice", 18),
+                new StudentsABC("Bob", 19),
+                new StudentsABC("Charlie", 18),
+                new StudentsABC("Anton", 23)
+        );
+
+        Map<String, List<StudentsABC>> group = groupByFirstLetter(students);
+
+        for (Map.Entry<String, List<StudentsABC>> entry : group.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+
+    }
+}
+
+class GroupStudentsByAlphabetAndAge {
+    public static Map<String, Double> groupByFirstLetter(List<StudentsABC> students) {
+        Map<String, List<Integer>> group = new TreeMap<>();
+
+        for (StudentsABC student : students) {
+            String firstLetter = student.getName().substring(0, 1).toUpperCase();
+            int age = student.getAge();
+
+            if (!group.containsKey(firstLetter)) {
+                group.put(firstLetter, new ArrayList<>());
+            }
+            group.get(firstLetter).add(age);
+
+        }
+
+        Map<String, Double> averageAge = new TreeMap<>();
+
+        for (Map.Entry<String, List<Integer>> entry : group.entrySet()) {
+            String letter = entry.getKey();
+            List<Integer> ages = entry.getValue();
+
+            int sum = 0;
+            for (int age : ages) {
+                sum += age;
+            }
+            double average = (double) sum / ages.size();
+            averageAge.put(letter, average);
+        }
+
+
+        return averageAge;
+    }
+
+    public static void main(String[] args) {
+        List<StudentsABC> students = Arrays.asList(
+                new StudentsABC("Alice", 18),
+                new StudentsABC("Anton", 20),
+                new StudentsABC("Bob", 21),
+                new StudentsABC("Boris", 19)
+
+        );
+        Map<String, Double> group = groupByFirstLetter(students);
+        for (Map.Entry<String, Double> entry : group.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
+
+class Contact {
+    private String name;
+    private String phone;
+
+    public Contact(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{name='" + name + "', phone='" + phone + "'}";
+    }
+
+}
+
+class MapContact {
+    public Map<String, String> map = new HashMap<>();
+
+    public void addContact(String name, String phone) {
+        Contact contact = new Contact(name, phone);
+        map.put(contact.getName(), contact.getPhone());
+        System.out.println("Контакт \"" + contact.getName() + "-->" + contact.getPhone() + "\" добавлен");
+    }
+
+    public boolean checkContact(String name) {
+        if (map.containsKey(name)) {
+            return true;
+        }
+        return false;
+
+    }
+
+    public void getPhone(String name) {
+        if (map.containsKey(name)) {
+            System.out.println("Номер контакта \"" + name + "\"-->" + map.get(name));
+        }
+    }
+
+    public void removeContact(String name) {
+        if (map.containsKey(name)) {
+            map.remove(name);
+            System.out.println("Контакт \"" + name + "\" удален");
+        }
+    }
+
+    public void showContact() {
+        System.out.println("Список контактов:");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        MapContact contacts = new MapContact();
+        contacts.addContact("Alex", "12345");
+        contacts.addContact("Bob", "67890");
+        contacts.addContact("Eva", "54321");
+
+        contacts.showContact();
+
+        System.out.println("Есть ли контакт с именем 'Bob'? " + contacts.checkContact("Bob"));
+
+        contacts.getPhone("Eva");
+
+        contacts.removeContact("Bob");
+
+        contacts.showContact();
+    }
+
+}
+
+class GroupContactsByNumber {
+    public static Map<String, List<String>> groupByFirstLetter(List<Contact> contacts) {
+        Map<String, List<String>> group = new HashMap<>();
+
+        for (Contact contact : contacts) {
+            String firstLetter = contact.getName().substring(0, 1).toUpperCase();
+            String number = contact.getPhone();
+            if (!group.containsKey(firstLetter)) {
+                group.put(firstLetter, new ArrayList<>());
+            }
+            group.get(firstLetter).add(number);
+        }
+        return group;
+    }
+
+    public static void main(String[] args) {
+        List<Contact> contacts = new ArrayList<>(Arrays.asList(
+                new Contact("Alice", "+12345"),
+                new Contact("Anton", "+99999"),
+                new Contact("Bob", "+23456"),
+                new Contact("Boris", "+88888"),
+                new Contact("Charlie", "+34567")
+        ));
+
+        Map<String, List<String>> group = groupByFirstLetter(contacts);
+
+        for (Map.Entry<String, List<String>> entry : group.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+    }
+}
+
+class HashMapStudy {
+    public static Map<String, List<Contact>> groupByFirstLetter(List<Contact> contacts) {
+        Map<String, List<Contact>> group = new TreeMap<>();
+
+        for (Contact contact : contacts) {
+            String firstLetter = contact.getName().substring(0, 1).toUpperCase();
+
+            if (!group.containsKey(firstLetter)) {
+                group.put(firstLetter, new ArrayList<>());
+            }
+            group.get(firstLetter).add(contact);
+        }
+        return group;
+    }
+
+    public static void main(String[] args) {
+        List<Contact> contacts = new ArrayList<>(Arrays.asList(
+                new Contact("Alice", "+12345"),
+                new Contact("Anton", "+99999"),
+                new Contact("Bob", "+23456"),
+                new Contact("Boris", "+88888"),
+                new Contact("Charlie", "+34567")
+        ));
+
+        Map<String, List<Contact>> group = groupByFirstLetter(contacts);
+
+        for (Map.Entry<String, List<Contact>> entry : group.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+    }
 }
