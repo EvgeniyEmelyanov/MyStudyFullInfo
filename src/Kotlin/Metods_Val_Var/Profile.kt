4dcfcc8c624f591e7e83f7cf7db49a1a2fc6a947
EvgeniyEmelyanov <jazenja4@gmail.com>
2025-09-01 21:27:47 +0300

    123

---
+++ src/Kotlin/Metods_Val_Var/Profile.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val inputName = "Input your name: "
+    println(inputName)
+    val name = readln()
+    println("Your name is : " + name)
+    println("Input your age")
+    val age = readln()
+    println("You are " + age + " ears old")
+    
+}
